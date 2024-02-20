3a87eb36b7451aeae80989575ebce32979d39014
freidavid <78372940+freidavid@users.noreply.github.com>
2024-02-20 14:58:24 +0100

    Update Add_to_geopackage.R

--- R/Add_to_geopackage.R
+++ R/Add_to_geopackage.R
@@ -4,7 +4,8 @@
 #'
 #' @param geopackage The complete path to the geopackage where the data should be added.
 #' @param data The path to the excel file from which the data is added to the geopackage file. The excel should contain all columns of the table of the geopackage in the same order. Additionally, there should be columns with the x and y coordinates ("X" and "Y") to generate the point geometry. 
-#' @param layer The layer of the 
+#' @param layer The layer of the geopackage, in which the data should be added to the datatable.
+#' @param crs The coordinate system to generate the geometry. Defaults to 2056.
 
 
 #' 
@@ -26,8 +27,7 @@
 
 
 
-
-add_to_geopackage<-function(geopackage,data,layer){
+add_to_geopackage<-function(geopackage,data,layer,crs=2506){
   
   #make geometry for the data frame
   for(i in 1:nrow(df)){
@@ -42,8 +42,10 @@ add_to_geopackage<-function(geopackage,data,layer){
     
     #add geometry to the data that should be added to the geopackage
     insert<-st_sf(df[i,], geometry = geometry)
+    st_crs(insert) <- st_crs(crs)
     
     #add to data with geometry to geopaackage
-    st_write(insert,geopackage,layer = layer,append=T)
+    st_write(insert,geopackage,layer = layer,append=T,driver="GPKG")
   }
 }
+
