960a74f72813f451203e19a0155edad375bd75ef
freidavid <78372940+freidavid@users.noreply.github.com>
2024-02-20 13:54:31 +0100

    Create Add_to_geopackage.R

---
+++ R/Add_to_geopackage.R
@@ -0,0 +1,49 @@
+
+
+#' Function to add data from excel to an existing table in a layer of a geopackage
+#'
+#' @param geopackage The complete path to the geopackage where the data should be added.
+#' @param data The path to the excel file from which the data is added to the geopackage file. The excel should contain all columns of the table of the geopackage in the same order. Additionally, there should be columns with the x and y coordinates ("X" and "Y") to generate the point geometry. 
+#' @param layer The layer of the 
+
+
+#' 
+#' @return Update geopackage
+#'
+#' @author David Frei, \email{david.frei@fornat.com}
+#'
+#'
+#' @import readxl
+#' @import gpkg
+#' @import sf
+#' @import tibble
+#' @export
+
+
+
+
+
+
+
+
+
+add_to_geopackage<-function(geopackage,data,layer){
+  
+  #make geometry for the data frame
+  for(i in 1:nrow(df)){
+    
+    #read data
+    df <- as.data.frame(read_excel(data))
+    #take x and y coordinates of every row of the data frame and make a geometry
+    geometry <- st_sfc(st_point(cbind(as.numeric(df[i,]$X), as.numeric(df[i,]$Y))))
+    
+    #delete x and y columns (not needed anymore)
+    df <- df[, !(names(df) %in% c("X","Y"))]
+    
+    #add geometry to the data that should be added to the geopackage
+    insert<-st_sf(df[i,], geometry = geometry)
+    
+    #add to data with geometry to geopaackage
+    st_write(insert,geopackage,layer = layer,append=T)
+  }
+}
